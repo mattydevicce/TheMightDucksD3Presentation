b4fd60689a9230e2c00058a9f60de37b6cb8b97e
Jacob Shemkovitz <jacob.shemkovitz@gmail.com>
2015-10-21 13:17:55 -0400

    initial setup

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 // Here we require Node.js libraries using CommonJS
 var express = require('express');
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var io = require('socket.io');
+var path = require('path');
 var app = express();
 
-app.use( express.static('public'));
+app.set('port', 3000);
 
-server.listen(3000);
+app.use('/', express.static(path.join(__dirname,'public')));
+
+app.listen(app.get('port'), function() {
+	console.log('App listening on http://localhost:%s', app.get('port'));
+});
