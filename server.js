c7f46e2d6f1c743a868408318268862703be6dff
Meiji Chen <meiji.chen@gmail.com>
2015-10-21 13:20:13 -0400

    commit before pull

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 var express = require('express');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var app = express();
-
+var path = require('path');
 app.use( express.static('public'));
+
+var app = express();
 
 server.listen(3000);
